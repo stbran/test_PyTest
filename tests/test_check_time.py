c9f536e6eae5960cb83e3be24891f87ac077fd1e
stbran <70580989+stbran@users.noreply.github.com>
2022-01-27 09:26:08 +0300

    Update test_check_time.py

--- tests/test_check_time.py
+++ tests/test_check_time.py
@@ -12,4 +12,4 @@ def test_checking_time():
     response = client.request('pool.ntp.org')
     server_time = datetime.datetime.fromtimestamp(response.tx_time)
     server_time_convert = datetime.timedelta(hours=server_time.hour, minutes=server_time.minute, seconds=server_time.second)
-    assert (server_time.hour - local_time.hour == 0 and server_time.minute - local_time.minute == 0 and server_time.second - local_time.second <= 2), "It's wrong local time"
+    assert (server_time.hour - local_time.hour == 0 and server_time.minute - local_time.minute == 0 and abs(server_time.second - local_time.second) <= 2), "It's wrong local time"
